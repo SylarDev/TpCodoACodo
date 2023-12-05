96d12f2788db1ffb93e0cfa300dd6c82065d367a
Uciel Espina <ucielespina93@gmail.com>
2023-12-05 01:40:21 -0300

    PRODUCTOS QUE ANDAN

--- app.py
+++ app.py
@@ -134,7 +134,9 @@ def menu():
 
 @app.route('/productos')
 def productos():
-    return render_template('productos.html')
+    lista_de_productos = producto_instance.listar_productos()
+    return render_template('productos.html', products=lista_de_productos)
+
 
 @app.route('/trabajaConNosotros')
 def trabajaConNosotros():
@@ -144,7 +146,70 @@ def trabajaConNosotros():
 def contactenos():
     return render_template('contactenos.html')
 
+
+@app.route('/agregar_producto', methods=['POST'])
+def agregar_producto():
+    if request.method == 'POST':
+        nueva_descripcion = request.form.get('nueva_descripcion')
+        nueva_cantidad = request.form.get('nueva_cantidad')
+        nuevo_precio = request.form.get('nuevo_precio')
+        nuevo_proveedor = request.form.get('nuevo_proveedor')
+
+        # Validar los campos (agrega más validaciones según sea necesario)
+
+        # Lógica para agregar un nuevo producto
+        # Por ejemplo, podrías utilizar tu instancia de Producto
+        producto_instance.agregar_producto(
+            codigo=None,  # Puedes dejar que la base de datos genere el código automáticamente
+            descripcion=nueva_descripcion,
+            cantidad=nueva_cantidad,
+            precio=nuevo_precio,
+            proveedor=nuevo_proveedor
+        )
+
+    return redirect(url_for('productos'))
+
+@app.route('/modificar_producto', methods=['POST'])
+def modificar_producto():
+    if request.method == 'POST':
+        codigo = request.form.get('codigo')
+        nueva_descripcion = request.form.get('nueva_descripcion')
+        nueva_cantidad = request.form.get('nueva_cantidad')
+        nuevo_precio = request.form.get('nuevo_precio')
+        nuevo_proveedor = request.form.get('nuevo_proveedor')
+
+        # Validar los campos (agrega más validaciones según sea necesario)
+
+        # Lógica para modificar un producto
+        # Por ejemplo, podrías utilizar tu instancia de Producto
+        producto_instance.modificar_producto(
+            codigo=codigo,
+            nueva_descripcion=nueva_descripcion,
+            nueva_cantidad=nueva_cantidad,
+            nuevo_precio=nuevo_precio,
+            nuevo_proveedor=nuevo_proveedor
+        )
+
+    return redirect(url_for('productos'))
+
+@app.route('/eliminar_producto', methods=['POST'])
+def eliminar_producto():
+    if request.method == 'POST':
+        codigo = request.form.get('codigo')
+
+        # Lógica para eliminar un producto
+        # Por ejemplo, podrías utilizar tu instancia de Producto
+        producto_instance.eliminar_producto(codigo)
+
+    return redirect(url_for('productos'))
+
+
 producto_instance = Producto(host='localhost', user='root', password='', database='miapp')
+
+producto_instance.agregar_producto(codigo=1, descripcion='Producto 1', cantidad=10, precio=19.99, proveedor=1)
+producto_instance.agregar_producto(codigo=2, descripcion='Producto 2', cantidad=5, precio=29.99, proveedor=2)
+producto_instance.agregar_producto(codigo=3, descripcion='Producto 3', cantidad=8, precio=39.99, proveedor=1)
+
 
 # run
 if __name__ == "__main__":
