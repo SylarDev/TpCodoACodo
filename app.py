8387c50deafec953d2ba3d8751e74258df33f838
Uciel Espina <ucielespina93@gmail.com>
2023-12-05 21:43:13 -0300

    andando

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-# # from flask_cors import CORS
-# from flaskext.mysql import MySQL
 
 import mysql.connector
-# # from werkzeug.utils import secure_filename
-# # import os
-# # import time
 
 # init
 app = Flask(__name__)
-
-# # Mysql Settings
-# mysql = MySQL()
-# app.config['MYSQL_DATABASE_HOST']     = 'localhost'
-# app.config['MYSQL_DATABASE_USER']     = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'tomba30db'
-# app.config['MYSQL_DATABASE_DB']       = 'flaskcontacts'
-# app.config['MYSQL_DATABASE_TABLE']    = 'productos'
-# mysql.init_app(app)
-
-
-
-# CORS(app)
 
 class Producto:
     
@@ -190,124 +172,16 @@ def borrar_producto(codigo):
 
         if producto_existente:
             producto_instance.eliminar_producto(codigo)
-            return jsonify({"mensaje": "Producto eliminado"}), 200
+            return redirect(url_for('productos'))  # Redirige a la página de productos después de la eliminación
         else:
-            return jsonify({"mensaje": "Producto no encontrado"}), 404
+            return render_template('producto_no_encontrado.html')  # O renderiza una plantilla de error
     else:
         return jsonify({"mensaje": "Código no proporcionado"}), 400
 
-
-# @app.route('/agregar_producto', methods=['POST'])
-# def agregar_producto():
-#     if request.method == 'POST':
-#         nueva_descripcion = request.form.get('nueva_descripcion')
-#         nueva_cantidad = request.form.get('nueva_cantidad')
-#         nuevo_precio = request.form.get('nuevo_precio')
-#         nuevo_proveedor = request.form.get('nuevo_proveedor')
-
-#         # Validar los campos (agrega más validaciones según sea necesario)
-
-#         # Lógica para agregar un nuevo producto
-#         # Por ejemplo, podrías utilizar tu instancia de Producto
-#         producto_instance.agregar_producto(
-#             codigo=None,  # Puedes dejar que la base de datos genere el código automáticamente
-#             descripcion=nueva_descripcion,
-#             cantidad=nueva_cantidad,
-#             precio=nuevo_precio,
-#             proveedor=nuevo_proveedor
-#         )
-
-#     return redirect(url_for('productos'))
-
-# @app.route('/modificar_producto')
-# def modificar_producto_html():
-#     return render_template('modificar_producto.html')
-
-# @app.route('/borrar_producto')
-# def borrar_producto_html():
-#     return render_template('borrar_producto.html')
-
-# # Carpeta para guardar las imagenes.
-# ruta_destino = './static/img/'
-# #--------------------------------------------------------------------
-# @app.route("/productos2", methods=["GET"])
-# def listar_productos():
-#     productos = producto_instance.listar_productos()
-#     return jsonify(productos)
-
-# #--------------------------------------------------------------------
-# @app.route("/productos2/<int:codigo>", methods=["GET"])
-# def mostrar_producto(codigo):
-#     producto = producto_instance.consultar_producto(codigo)
-#     if producto:
-#         return jsonify(producto)
-#     else:
-#         return "Producto no encontrado", 404
-
-# #--------------------------------------------------------------------
-
-# @app.route("/productos", methods=["POST"])
-# def agregar_producto():
-    
-#     codigo = request.form.get('codigo')
-#     descripcion = request.form.get('descripcion')
-#     cantidad = request.form.get('cantidad')
-#     precio = request.form.get('precio')
-#     proveedor = request.form.get('proveedor')  
-    
-#     if producto_instance.agregar_producto(codigo, descripcion, cantidad, precio, proveedor):
-#         return jsonify({"mensaje": "Producto agregado"}), 201
-#     else:
-#         return jsonify({"mensaje": "Producto ya existe"}), 400
-
-# #--------------------------------------------------------------------
-# @app.route('/modificar_producto', methods=['POST'])
-# def modificar_producto():
-#     codigo = request.form.get('codigo_modificar')
-#     nueva_descripcion = request.form.get('nueva_descripcion')
-#     nueva_cantidad = request.form.get('nueva_cantidad')
-#     nuevo_precio = request.form.get('nuevo_precio')
-#     nuevo_proveedor = request.form.get('nuevo_proveedor')
-
-#     if producto_instance.consultar_producto(codigo):
-#          if producto_instance.modificar_producto(codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nuevo_proveedor):
-             
-#             return jsonify({'mensaje': 'producto modificado'}), 201
-       
-#     else:
-#         return jsonify({'mensaje': 'producto no modificado'}),400
-       
-     
-# #--------------------------------------------------------------------
-
-# @app.route('/borrar_producto', methods=['POST'])
-# def borrar_producto():
-#     codigo = request.form.get('codigo_eliminar')
-#     if codigo:
-#         producto_existente = producto_instance.consultar_producto(codigo)
-
-#         if producto_existente:
-#             producto_instance.eliminar_producto(codigo)
-#             return jsonify({"mensaje": "Producto eliminado"}), 200
-#         else:
-#             return jsonify({"mensaje": "Producto no encontrado"}), 404
-#     else:
-#         return jsonify({"mensaje": "Código no proporcionado"}), 400
-   
 #--------------------------------------------------------------------
 
 
 producto_instance = Producto(host='localhost', user='root', password='', database='miapp')
-
-producto_instance.agregar_producto(codigo=1, descripcion='Producto 1', cantidad=10, precio=19.99, proveedor=1)
-producto_instance.agregar_producto(codigo=2, descripcion='Producto 2', cantidad=5, precio=29.99, proveedor=2)
-producto_instance.agregar_producto(codigo=3, descripcion='Producto 3', cantidad=8, precio=39.99, proveedor=1)
-producto_instance.agregar_producto(codigo=1, descripcion='Producto 4', cantidad=10, precio=19.99, proveedor=1)
-producto_instance.agregar_producto(codigo=2, descripcion='Producto 5', cantidad=5, precio=29.99, proveedor=2)
-producto_instance.agregar_producto(codigo=3, descripcion='Producto 6', cantidad=8, precio=39.99, proveedor=1)
-producto_instance.agregar_producto(codigo=1, descripcion='Producto 7', cantidad=10, precio=19.99, proveedor=1)
-producto_instance.agregar_producto(codigo=2, descripcion='Producto 8', cantidad=5, precio=29.99, proveedor=2)
-producto_instance.agregar_producto(codigo=3, descripcion='Producto 9', cantidad=8, precio=39.99, proveedor=1)
 
 # run
 if __name__ == "__main__":
